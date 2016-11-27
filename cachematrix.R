c3484d210ad9152d817f1aeebf5e971660fd6343
ZeroProfanity <martjandenhollander@gmail.com>
2016-11-27 22:45:05 +0100

    Assignment course 2
    R Programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In order to reduce calculation time on performing the same matrix
+## inversion twice, the functions below allow for a inverted matrix
+## to be cached. Only the first inversion by cacheSolve(x) will 
+## execute a calculation where all subsequent inversions of the same 
+## matrix will result in a read from the cache.
 
-## Write a short comment describing this function
-
+## Converts a matrix to a cacheable matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inv) m <<- inv
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
+## Reads a previously cached inverted cacheable matrix if this 
+## matrix is available in cache. If this is not the case, the  
+## cacheable matrix is inverted and committed to cache for future
+## reads.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
